8e7188b3e3fb6b54301cb11a0e681e5311fe527d
Francisco Coelho <franciscocoelho.ti@gmail.com>
2026-09-11 11:35:44 -0300

    Construção do app Acadêmico, e os modelos iniciais

---
+++ academico/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import AnoLetivo,Turma,Aluno
+
+admin.site.register(AnoLetivo)
+admin.site.register(Turma)
+admin.site.register(Aluno)
+
+
---
+++ academico/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from datetime import datetime
+
+class AnoLetivo(models.Model):
+    ano = models.IntegerField(default=datetime.now().year)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return str(self.ano)
+
+class Turma(models.Model):
+    id = models.AutoField(primary_key=True)
+    descricao = models.CharField(max_length=75)
+    ano_letivo = models.ForeignKey(
+        AnoLetivo, 
+        on_delete=models.PROTECT
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.descricao
+
+class Aluno(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11)
+    ra = models.CharField(max_length=11)
+    nome_responsavel = models.CharField(max_length=50)
+    telefone_responsavel = models.CharField(max_length=20)
+    turma = models.ForeignKey(
+        Turma,
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self) -> str:
+        return self.nome
